95bdf8eb6f054716146dc8e3e8848ff9f912c045
acassaigne <anthony.cassaigne@gmail.com>
2021-05-16 10:47:15 +0200

    Add manipulate vector cons/conj. And refactoring some parts.

--- hello_example/src/hello_example/core.clj
+++ hello_example/src/hello_example/core.clj
@@ -53,16 +53,38 @@
   )
 
 
-(defn main-vector []
+;; create vectors
+(defn examples-create-vectors []
   (println (create-simple-vector))
   (println (create-more-complex-vector))
   (println (create-a-vector))
   (print-count "complex" (create-more-complex-vector))
   (print-count "simple" (create-a-vector))
   (println "create an empty vector" (vector))
+  )
+
+;; manipulate vector first, rest, nth
+(defn example-manipulate-vector []
   (println "The first element of vector is...." (first (create-simple-vector)))
   (println "The rest of vector is...." (rest (create-simple-vector)))
+  (println "Return 3th value (index start at zero)..." (nth ["a1" "b2" "c3" "d4" "e5"] 2))
   )
+
+;; growing vectors
+(defn example-growing-vector []
+  (println "Conjunction for one vector (add element to the end..." (conj ["a" "b"] "c"))
+  ;; cons return a sequence
+  (println "Construction for one vector (add element at the first position..." (cons "a"  [ "b" "c"]))
+  )
+
+(defn main-vector []
+  (examples-create-vectors)
+  (example-manipulate-vector)
+  (example-growing-vector)
+  )
+
+;; (rest ["a" "b" "c"]) retourne une sequence et non pas un vector
+;; sequence = sort of generic collection
 
 ;; main
 (defn -main
